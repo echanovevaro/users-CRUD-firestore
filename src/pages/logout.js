769e3fbf492158d9fda3ef91e3d7207487e36ccc
echanovevaro <echanovevaro@gmail.com>
2023-12-04 11:48:34 +0100

    fix logout

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,7 +1,14 @@
 import { redirect } from "react-router-dom";
+import { auth } from "../firebase";
+import { signOut } from "firebase/auth";
 
-export const action = () => {
+export const action = async () => {
     localStorage.removeItem('token');
     localStorage.removeItem('expiration');
+    try {
+        await signOut(auth);
+    } catch (error) {
+        throw new Error('An error ocurred while loging out', {status: 500});
+    }
     return redirect('/auth?mode=login');
 }
